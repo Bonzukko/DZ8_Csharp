5a16091faf076fd8cfdf25e39c296e2bf11c696b
Dmitry Fedotov <fedotov-ds1989@yandex.ru>
2022-09-18 23:54:32 +0300

    task056

---
+++ task_056/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+Random random = new Random();
+int[,] matrix = new int[random.Next(2, 6), random.Next(2, 6)];
+int minSum = int.MaxValue;
+int result = 0;
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    int sum = 0;
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = random.Next(0, 100);
+        Console.Write(matrix[i, j] + " ");
+        sum += matrix[i, j];
+    }
+    Console.WriteLine();
+    if (sum < minSum)
+    {
+        minSum = sum;
+        result = i + 1;
+    }
+}
+Console.WriteLine($"номер строки с наименьшей суммой элементов: {result}");
